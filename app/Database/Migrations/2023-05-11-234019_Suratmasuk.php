16244447f72e7d3159c47ee0013604fffca670de
IndoproGMR <indoprogamer007@gmail.com>
2023-08-10 02:11:43 +0700

    penambahan database

--- app/Database/Migrations/2023-05-11-234019_Suratmasuk.php
+++ app/Database/Migrations/2023-05-11-234019_Suratmasuk.php
@@ -4,70 +4,49 @@ namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
+$GLOBALS['dbprefix'] = 'SM_';
+$GLOBALS['attributes'] = ['ENGINE' => 'InnoDB'];
+
 class Suratmasuk extends Migration
 {
     public function up()
     {
-        $dbprefix = "SM_";
-
-
-        // !JenisSurat
-        $tablee = "JenisSurat";
+        // !JenisSuratAr
+        $tablee = "JenisSuratAr";
         $fields = [
-            'id'          => ['type' => 'int', 'constraint'     => 11, 'unsigned' => true, 'auto_increment' => true],
-            'name'        => ['type' => 'varchar', 'constraint' => 255],
-            'description' => ['type' => 'varchar', 'constraint' => 255],
-            'isiSurat'    => ['type' => 'text'],                                            //json base64
-            'form'        => ['type' => 'text'],                                            //json base64
-            'show'        => ['type' => 'tinyint', 'constraint' => 1,  'default'  => 0],    // di show kepada user
-            'delete'      => ['type' => 'int', 'constraint'     => 12, 'default'  => null]
+            'id' => [
+                'type'           => 'int',
+                'constraint'     => 10,
+                'unsigned'       => true,
+                'auto_increment' => true
+            ],
+            'name' => [
+                'type'       => 'varchar',
+                'constraint' => 255
+            ],
+            'description' => [
+                'type'       => 'varchar',
+                'constraint' => 255
+            ],
+            'TimeStamp' => [
+                'type'       => 'int',
+                'constraint' => 10,
+                'unsigned'   => true
+            ],
+            'DeleteAt' => [
+                'type'       => 'int',
+                'constraint' => 10,
+                'default'    => null
+            ],
         ];
         $this->forge->addField($fields);
         $this->forge->addKey('id', true);
-        $this->forge->createTable("$dbprefix" . "$tablee", true);
-
-
-        // !ttd-SuratMasuk
-        $tablee = "ttd_SuratMasuk";
-        $fields = [
-            'id'            => ['type' => 'int', 'constraint'     => 11, 'unsigned' => true, 'auto_increment' => true],
-            'NoSurat'       => ['type' => 'varchar', 'constraint' => 64],
-            'DataTambahan'  => ['type' => 'text'],                         //json base64
-            'TimeStamp'     => ['type' => 'int', 'constraint' => 12],
-            'JenisSurat_id' => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'default' => 0],
-            'mshw_id'       => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'default' => 0],
-            'delete'        => ['type' => 'int', 'constraint'     => 12, 'default'  => null]
-        ];
-        $this->forge->addField($fields);
-        $this->forge->addKey('id', true);
-        $this->forge->addForeignKey('JenisSurat_id', "$dbprefix" . 'JenisSurat', 'id', '', 'CASCADE');
-        $this->forge->createTable("$dbprefix" . "$tablee", true);
-
-
-        // !ttd
-        $tablee = "ttd";
-        $fields = [
-            'id'          => ['type' => 'int', 'constraint'     => 11, 'unsigned' => true, 'auto_increment' => true],
-            'NoSurat'     => ['type' => 'varchar', 'constraint' => 8,  'default'  => 0],
-            'Status'      => ['type' => 'tinyint', 'constraint' => 1,  'default'  => 0],
-            'hash'        => ['type' => 'text', 'default'       => NULL],
-            'qrcodeName'  => ['type' => 'varchar', 'constraint' => 30, 'default' => NULL],  // QRcode name untuk di panggil
-            'jenisttd'    => ['type' => 'varchar', 'constraint' => 12],                     // group apa perorangan
-            'pendattg_id' => ['type' => 'varchar', 'constraint' => 20, 'default'  => NULL], // nama grup atau id login
-            'TimeStamp'   => ['type' => 'int', 'constraint'     => 12, 'default'  => 0],
-        ];
-        $this->forge->addField($fields);
-        $this->forge->addKey('id', true);
-        $this->forge->createTable("$dbprefix" . "$tablee", true);
+        $this->forge->createTable($GLOBALS['dbprefix'] . "$tablee", true, $GLOBALS['attributes']);
     }
 
     public function down()
     {
-        $dbprefix = "SM_";
-
-        $this->forge->dropTable($dbprefix . 'JenisSurat', true);
-        $this->forge->dropTable($dbprefix . 'ttd_SuratMasuk', true);
-        $this->forge->dropTable($dbprefix . 'ttd', true);
+        $this->forge->dropTable($GLOBALS['dbprefix'] . 'JenisSuratAr', true);
     }
 }
 
