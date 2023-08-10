0d2fd2291c7115580d15a4db31261185efef9095
IndoproGMR <indoprogamer007@gmail.com>
2023-08-11 01:34:11 +0700

    penambahan database

--- app/Database/Migrations/2023-05-11-234019_Suratmasuk.php
+++ app/Database/Migrations/2023-05-11-234019_Suratmasuk.php
@@ -12,7 +12,7 @@ class Suratmasuk extends Migration
     public function up()
     {
         // !JenisSuratAr
-        $tablee = "JenisSuratAr";
+        $tablee = "JenisSuratArchice";
         $fields = [
             'id' => [
                 'type'           => 'int',
@@ -42,10 +42,55 @@ class Suratmasuk extends Migration
         $this->forge->addField($fields);
         $this->forge->addKey('id', true);
         $this->forge->createTable($GLOBALS['dbprefix'] . "$tablee", true, $GLOBALS['attributes']);
+
+
+        // !SuratMasuk
+        $tablee = "SuratArchice";
+        $fields = [
+            'id' => [
+                'type'       => 'varchar',
+                'constraint' => 16,
+            ],
+            'NomerSurat' => [
+                'type'       => 'varchar',
+                'constraint' => 255
+            ],
+            'TanggalSurat' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'DataSurat' => [
+                'type'       => 'TEXT'
+            ],
+            'NamaFile' => [
+                'type'       => 'varchar',
+                'constraint' => 16
+            ],
+            'JenisSuratArchice_id' => [
+                'type'       => 'int',
+                'constraint' => 10,
+                'default'    => 0
+            ],
+            'TimeStamp' => [
+                'type'       => 'int',
+                'constraint' => 10,
+                'unsigned'   => true
+            ],
+            'DeleteAt' => [
+                'type'       => 'int',
+                'constraint' => 10,
+                'default'    => null
+            ],
+        ];
+        $this->forge->addField($fields);
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('JenisSuratArchice_id', $GLOBALS['dbprefix'] . 'JenisSuratArchice', 'id', 'CASCADE', 'SET DEFAULT');
+        $this->forge->createTable($GLOBALS['dbprefix'] . "$tablee", true, $GLOBALS['attributes']);
     }
 
     public function down()
     {
-        $this->forge->dropTable($GLOBALS['dbprefix'] . 'JenisSuratAr', true);
+        $this->forge->dropTable($GLOBALS['dbprefix'] . 'JenisSuratArchice', true);
+        $this->forge->dropTable($GLOBALS['dbprefix'] . 'SuratArchice', true);
     }
 }
