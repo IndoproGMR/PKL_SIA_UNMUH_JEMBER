91dbbb8d50dbe5a6c893da2a2ff0da2ec8598167
IndoproGMR <indoprogamer007@gmail.com>
2023-08-27 21:20:04 +0700

    perbaikan total css

--- app/Views/suratkeluar/index_SuratTanpaNo.php
+++ app/Views/suratkeluar/index_SuratTanpaNo.php
@@ -1,68 +1,71 @@
 <?= $this->extend('templates/layout.php') ?>
 <?= $this->section('style') ?>
-<link rel="stylesheet" href="<?= base_url('/'); ?>css/status.css">
+<link rel="stylesheet" href="<?= base_url('/'); ?>css/tablestyle.css">
+
 <?= $this->endSection() ?>
 
 <?= $this->section('main') ?>
 
+
 <div class="filter">
     <p class="first">Filter:</p>
-    <p class="second">Jenis Surat</p>
+    <!-- <p class="second">Jenis Surat: </p> -->
     <form>
-        <select>
-            <option>PKL</option>
-            <option>Surat Mahasiswa Aktif</option>
-            <option>Surat Dispen</option>
+        <select name="" id="">
+            <option value="">a</option>
+            <option value="">b</option>
+            <option value="">aaaaaaaaaaaaaaaa bbbbbbbb ccccccccccccc</option>
         </select>
+        <input type="submit" value="Cari">
     </form>
-    <p class="third">Tanggal</p>
+    <p class="Time">Tanggal: <span class="waktu-sekarang"></span></p>
 </div>
-<div class="table-rown">
-    <table>
-        <thead>
+
+
+<table>
+    <thead>
+        <tr>
+            <th>no</th>
+            <th>NoSurat</th>
+            <th>jenis Surat</th>
+            <th>TimeStamp</th>
+            <th>nama mahasiswa</th>
+            <th>Actions</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <?php foreach ($datasurat as $index => $key) : ?>
             <tr>
-                <th>no</th>
-                <th>NoSurat</th>
-                <th>jenis Surat</th>
-                <th>TimeStamp</th>
-                <th>nama mahasiswa</th>
-                <th>Actions</th>
+                <td class="num"><?= esc($index + 1) ?></td>
+                <td><?= esc($key['NoSurat']) ?></td>
+                <td><?= esc($key['name']) ?></td>
+                <td><?= esc(timeconverter($key['TimeStamp'])) ?></td>
+                <td><?= esc($key['namaMahasiswa']['Nama']) ?></td>
+                <td>
+                    <?= view_cell('TombolIdCell', [
+                        'link'              => 'edit/surat-tanpa_NoSurat',
+                        'valueinput'        => $key['id'],
+                        'tombolsubmitclass' => 'Actions',
+                        'textsubmit'        => 'Edit Surat',
+                        'confirmdialog'     => true,
+                        'textConfirm'       => 'edit Surat ini ?',
+                        'target'            => '_self'
+                    ]) ?>
+
+                    <?= view_cell('TombolIdCell', [
+                        'link'              => 'staff/Preview-Surat',
+                        'valueinput'        => $key['id'],
+                        'tombolsubmitclass' => 'Actions',
+                        'textsubmit'        => 'Preview Surat',
+                        'confirmdialog'     => false,
+                        'target'            => '_blank'
+                    ]) ?>
+                </td>
             </tr>
-        </thead>
+        <?php endforeach ?>
+    </tbody>
+</table>
 
-        <tbody>
-            <?php foreach ($datasurat as $index => $key) : ?>
-                <tr>
-                    <td><?= esc($index + 1) ?></td>
-                    <td><?= esc($key['NoSurat']) ?></td>
-                    <td><?= esc($key['name']) ?></td>
-                    <!-- <td><?= TombolID('staff/Preview-Surat', $key['NoSurat'], 'signature', 'Preview Surat', '_blank') ?></td> -->
-                    <td><?= esc(timeconverter($key['TimeStamp'])) ?></td>
-                    <td><?= esc($key['namaMahasiswa']['Nama']) ?></td>
-                    <td>
-                        <?= view_cell('TombolIdCell', [
-                            'link'              => 'edit/surat-tanpa_NoSurat',
-                            'valueinput'        => $key['id'],
-                            'tombolsubmitclass' => 'signature',
-                            'textsubmit'        => 'Edit Surat',
-                            'confirmdialog'     => true,
-                            'textConfirm'       => 'edit Surat ini ?',
-                            'target'            => '_self'
-                        ]) ?>
-
-                        <?= view_cell('TombolIdCell', [
-                            'link'              => 'staff/Preview-Surat',
-                            'valueinput'        => $key['id'],
-                            'tombolsubmitclass' => 'signature',
-                            'textsubmit'        => 'Preview Surat',
-                            'confirmdialog'     => false,
-                            'target'            => '_blank'
-                        ]) ?>
-                    </td>
-                </tr>
-            <?php endforeach ?>
-        </tbody>
-    </table>
-</div>
 
 <?= $this->endSection() ?>
